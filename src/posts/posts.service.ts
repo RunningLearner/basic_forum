211f7f471cad01f3cdb00e7ee50d14c54891975c
seunam <seunam@student.42seoul.kr>
2022-10-02 17:12:44 +0900

    feat: createPost service added

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -12,6 +12,25 @@ export class PostsService {
   ) {}
 
   async createPost(postInfo: PostInfoDto) {
+    const post = new Post();
+
+    post.title = postInfo.title;
+    post.content = postInfo.content;
+    post.hashtags = postInfo.hashtags;
+    await this.postRepository.save(post);
+
+    return { message: '회원가입이 완료되었습니다.' };
+  }
+  async getAllPosts() {
+    return;
+  }
+  async getPost(postId: number) {
+    return;
+  }
+  async updatePost(postId: number, postInfo: PostInfoDto) {
+    return;
+  }
+  async deletePost(postId: number) {
     return;
   }
 }

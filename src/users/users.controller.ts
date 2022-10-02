5c21a5faa03faed654ef7995d79d796916d60fa4
seunam <seunam@student.42seoul.kr>
2022-10-02 16:44:50 +0900

    feat: createuser controller added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() email: string) {
+    return this.usersService.createUser(email);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(email: string) {
+    const user = new User();
+
+    user.email = email;
+    await this.userRepository.save(user);
+
+    return { message: '회원가입이 완료되었습니다.' };
+  }
+}
